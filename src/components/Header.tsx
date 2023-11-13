1cd64ad890389b7a201aab54ccf12528d15d4607
YannickVandereycken <91282328+YannickVandereycken@users.noreply.github.com>
2023-11-13 18:45:55 +0100

    poging logo

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,5 +1,5 @@
 import React from "react";
-import { View, Text, Button } from "react-native";
+import { View, Text, Button, Image } from "react-native";
 import Icon from 'react-native-vector-icons/FontAwesome'
 
 import styles from "./Styles";
@@ -7,6 +7,7 @@ import styles from "./Styles";
 const Header: React.FC = () => {
     return (
         <View style={styles.header}>
+            {/* <Image source={require('/src/assets/logo.jpg')} /> */}
             <Text style={styles.title}>GameHoard</Text>
             {/* <Icon name='facebook' size={20} color='firebrick' /> */}
             <Text style={styles.text}>Star</Text>
