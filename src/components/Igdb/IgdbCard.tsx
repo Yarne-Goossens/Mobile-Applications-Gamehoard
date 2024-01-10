23c794e68d9be7ce338af4020ab08486948b0587
YannickVandereycken <91282328+YannickVandereycken@users.noreply.github.com>
2024-01-10 17:49:32 +0100

    small updates to igdb and add critic and user rating to game

--- src/components/Igdb/IgdbCard.tsx
+++ src/components/Igdb/IgdbCard.tsx
@@ -5,7 +5,9 @@ import { Card } from '@rneui/themed';
 import styles from "../constants/Styles";
 import { Game } from "../../types/types";
 import Icon from "react-native-vector-icons/FontAwesome";
+import MIcon from "react-native-vector-icons/MaterialIcons";
 import gameService from "../../services/game.service";
+import { colors } from "../constants/Constants";
 
 type Props = {
     games: Array<Game> | undefined;
@@ -23,7 +25,7 @@ const GameView = ({ game, navigation, updateScreen }: GameProps) => (
     <Card wrapperStyle={styles.gamecard} containerStyle={styles.gamecardContainer}>
         <View style={{ flexDirection: 'row', justifyContent: 'space-evenly' }}>
             <Card.Title style={styles.gameTitle}>{game.name}</Card.Title>
-            {game.favorite ? <Icon style={{ paddingLeft: 60, paddingTop: 5 }} name='heart' size={30} color='darkslateblue' onPress={async () => { await gameService.favoriteGame(game.game_id); updateScreen() }} /> : <Icon style={{ paddingLeft: 60, paddingTop: 5 }} name='heart-o' size={30} color='darkslateblue' onPress={async () => { await gameService.favoriteGame(game.game_id); updateScreen() }} />}
+            {game.favorite ? <Icon style={{ paddingLeft: 60, paddingTop: 5 }} name='heart' size={30} color={colors.iconFavorite} onPress={async () => { await gameService.favoriteGame(game.game_id); updateScreen() }} /> : <Icon style={{ paddingLeft: 60, paddingTop: 5 }} name='heart-o' size={30} color={colors.iconFavorite} onPress={async () => { await gameService.favoriteGame(game.game_id); updateScreen() }} />}
         </View>
         <Card.Divider />
         <View style={styles.gamecardInternal}>
@@ -35,12 +37,15 @@ const GameView = ({ game, navigation, updateScreen }: GameProps) => (
                 />
             </View>
             <View style={{ width: "65%" }}>
-                <Text style={styles.textGame}>Id: {game.game_id}</Text>
-                {/*<Text style={styles.textGenre}>Genre(s): {game.genre.toString()}</Text>*/}
-                <Text style={styles.textGame}>Price: {game.price} / {game.msrp ? game.msrp : 'NA'} €</Text>
-                <Text style={styles.textGame}><Icon name="clock-o" size={20} /> {game.playtime ? game.playtime : 'NA'}</Text>
+                <Text style={styles.textGame}>IGDB ID: {game.game_id}</Text>
+                <Text style={styles.textGenre}>Genre(s): Indie, Adventure </Text>
+                {/* {game.genre.toString()} */}
+                {/* <Text style={styles.textGame}>Price: {game.price} / {game.msrp ? game.msrp : 'NA'} €</Text> */}
+                <Text style={styles.textGame}>User Rating: {game.user_rating ? (game.user_rating / 10).toFixed(2) : <Icon name="eye-slash" size={20} />}/10</Text>
+                <Text style={styles.textGame}>Critic Rating: {game.critic_rating ? (game.critic_rating).toFixed(2) : <Icon name="eye-slash" size={20} />}</Text>
+                <Text style={styles.textGame}>Multiplayer: {game.multiplayer ? <MIcon name="people" size={20} /> : <Icon name="eye-slash" size={20} />}</Text>
                 <Button
-                    title="Go to Details"
+                    title="Add to Library"
                     onPress={() => navigation.navigate('Details', { gameId: game.game_id })}
                 />
             </View>
