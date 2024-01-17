130b8d7bee9ff2d17f02f312ed8bc40130aacfe6
YannickVandereycken <91282328+YannickVandereycken@users.noreply.github.com>
2024-01-17 19:35:01 +0100

    add more platform tabs and order the platforms more logical

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -18,11 +18,16 @@ const Footer: React.FC<Props> = ({ navigation }: Props) => {
             <ScrollView horizontal={true} style={stylesLocal.tabList} contentInsetAdjustmentBehavior="automatic">
                 <Text style={styles.tabtext} onPress={() => navigation.navigate('Home', { update: true })}>All</Text>
                 <MCIcon style={styles.iconTab} name="steam" onPress={() => navigation.navigate('Platforms', { platform: "Steam" })} />
+                <IIcon style={styles.iconTab} name="nuclear" onPress={() => navigation.navigate('Platforms', { platform: "Epic" })} />
+                <MCIcon style={styles.iconTab} name="gog" onPress={() => navigation.navigate('Platforms', { platform: "GOG" })} />
                 <MCIcon style={styles.iconTab} name="nintendo-switch" onPress={() => navigation.navigate('Platforms', { platform: "Nintendo" })} />
-                <IIcon style={styles.iconTab} name="logo-apple" onPress={() => navigation.navigate('Platforms', { platform: "Mac" })} />
-                <IIcon style={styles.iconTab} name="logo-windows" onPress={() => navigation.navigate('Platforms', { platform: "Windows" })} />
                 <IIcon style={styles.iconTab} name="logo-playstation" onPress={() => navigation.navigate('Platforms', { platform: "Playstation" })} />
                 <IIcon style={styles.iconTab} name="logo-xbox" onPress={() => navigation.navigate('Platforms', { platform: "XBox" })} />
+                <IIcon style={styles.iconTab} name="logo-apple" onPress={() => navigation.navigate('Platforms', { platform: "macOS" })} />
+                <IIcon style={styles.iconTab} name="logo-windows" onPress={() => navigation.navigate('Platforms', { platform: "Windows" })} />
+                <IIcon style={styles.iconTab} name="logo-tux" onPress={() => navigation.navigate('Platforms', { platform: "Linux" })} />
+                <MCIcon style={styles.iconTab} name="apple-ios" onPress={() => navigation.navigate('Platforms', { platform: "iOS" })} />
+                <IIcon style={styles.iconTab} name="logo-android" onPress={() => navigation.navigate('Platforms', { platform: "Android" })} />
             </ScrollView>
         </View>
     )
